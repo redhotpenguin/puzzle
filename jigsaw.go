90cabe7bfc4f2329f7c9e04bca699746380caab5
Fred Moyer <fred@redhotpenguin.com>
2023-10-27 21:34:46 -0700

    flip between innie and outie piece layout

--- jigsaw.go
+++ jigsaw.go
@@ -11,11 +11,11 @@ type point struct {
 	y float32
 }
 
-func flip() int {
-	if rand.Intn(1) == 0 {
-		return 1
+func flip() float32 {
+	if rand.Intn(2) == 0 {
+		return 1.0
 	} else {
-		return -1
+		return -1.0
 	}
 }
 
@@ -31,56 +31,60 @@ func main() {
 	var curveThree float32 = curveOne * 2 // 0.4
 	var curveFour float32 = curveTwo * 3  // 0.3
 
-	// start with the scenario of making the piece with all knubs outward facing
-	// top side
+	// outie or innie??
+	var outie float32 = flip() // 1.0 is outie, -1.0 is innie
+
+	fmt.Printf("<!-- Outie %1.0f -->\n", outie)
+
+	// top side. Every 3 lines is one C SVG declaration
 	curves[0][0][0] = point{size * curveOne, 0}
-	curves[0][0][1] = point{size * halfway, size * curveTwo}
-	curves[0][0][2] = point{size * curveThree, size * curveTwo * -1}
+	curves[0][0][1] = point{size * halfway, size * outie * curveTwo}
+	curves[0][0][2] = point{size * curveThree, size * outie * curveTwo * -1}
 
-	curves[0][1][0] = point{size * curveFour, -1 * size * curveFour}
-	curves[0][1][1] = point{size * (1 - curveFour), -1 * size * curveFour}
-	curves[0][1][2] = point{size * (1 - curveThree), size * curveTwo * -1}
+	curves[0][1][0] = point{size * curveFour, size * outie * curveFour * -1}
+	curves[0][1][1] = point{size * (1 - curveFour), size * outie * curveFour * -1}
+	curves[0][1][2] = point{size * (1 - curveThree), size * outie * curveTwo * -1}
 
-	curves[0][2][0] = point{size * halfway, size * curveTwo}
+	curves[0][2][0] = point{size * halfway, size * outie * curveTwo}
 	curves[0][2][1] = point{size * (1 - curveOne), 0}
 	curves[0][2][2] = point{size, 0}
 
 	// right side
 	curves[1][0][0] = point{size, size * curveOne}
-	curves[1][0][1] = point{size * (1 - curveTwo), size * halfway}
-	curves[1][0][2] = point{size * (1 + curveTwo), size * 2 * curveOne}
+	curves[1][0][1] = point{size * (1 - (outie * curveTwo)), size * halfway}
+	curves[1][0][2] = point{size * (1 + (outie * curveTwo)), size * 2 * curveOne}
 
-	curves[1][1][0] = point{size * (1 + curveFour), size * curveFour}
-	curves[1][1][1] = point{size * (1 + curveFour), size * (1 - curveFour)}
-	curves[1][1][2] = point{size * (1 + curveTwo), size * (1 - curveThree)}
+	curves[1][1][0] = point{size * (1 + (outie * curveFour)), size * curveFour}
+	curves[1][1][1] = point{size * (1 + (outie * curveFour)), size * (1 - curveFour)}
+	curves[1][1][2] = point{size * (1 + (outie * curveTwo)), size * (1 - curveThree)}
 
-	curves[1][2][0] = point{size * (1 - curveTwo), size * halfway}
+	curves[1][2][0] = point{size * (1 - (outie * curveTwo)), size * halfway}
 	curves[1][2][1] = point{size, size * (1 - curveOne)}
 	curves[1][2][2] = point{size, size}
 
 	// bottom side
 	curves[2][0][0] = point{size * (1 - curveOne), size}
-	curves[2][0][1] = point{size * halfway, size * (1 - curveTwo)}
-	curves[2][0][2] = point{size * (1 - curveThree), size * (1 + curveTwo)}
+	curves[2][0][1] = point{size * halfway, size * (1 - (outie * curveTwo))}
+	curves[2][0][2] = point{size * (1 - curveThree), size * (1 + (outie * curveTwo))}
 
-	curves[2][1][0] = point{size * (1 - curveFour), size * (1 + curveFour)}
-	curves[2][1][1] = point{size * curveFour, size * (1 + curveFour)}
-	curves[2][1][2] = point{size * curveThree, size * (1 + curveTwo)}
+	curves[2][1][0] = point{size * (1 - curveFour), size * (1 + (outie * curveFour))}
+	curves[2][1][1] = point{size * curveFour, size * (1 + (outie * curveFour))}
+	curves[2][1][2] = point{size * curveThree, size * (1 + (outie * curveTwo))}
 
-	curves[2][2][0] = point{size * halfway, size * (1 - curveTwo)}
+	curves[2][2][0] = point{size * halfway, size * (1 - (outie * curveTwo))}
 	curves[2][2][1] = point{size * (1 * curveOne), size}
 	curves[2][2][2] = point{0, size}
 
 	// left side
 	curves[3][0][0] = point{0, size * (1 - curveOne)}
-	curves[3][0][1] = point{size * curveTwo, size * halfway}
-	curves[3][0][2] = point{size * (-1 * curveTwo), size * (1 - curveThree)}
+	curves[3][0][1] = point{size * outie * curveTwo, size * halfway}
+	curves[3][0][2] = point{size * (-1 * outie * curveTwo), size * (1 - curveThree)}
 
-	curves[3][1][0] = point{size * (-1 * curveFour), size * (1 - curveFour)}
-	curves[3][1][1] = point{size * (-1 * curveFour), size * curveFour}
-	curves[3][1][2] = point{size * (-1 * curveTwo), size * curveThree}
+	curves[3][1][0] = point{size * (-1 * outie * curveFour), size * (1 - curveFour)}
+	curves[3][1][1] = point{size * (-1 * outie * curveFour), size * curveFour}
+	curves[3][1][2] = point{size * (-1 * outie * curveTwo), size * curveThree}
 
-	curves[3][2][0] = point{size * curveTwo, size * halfway}
+	curves[3][2][0] = point{size * outie * curveTwo, size * halfway}
 	curves[3][2][1] = point{0, size * curveOne}
 	curves[3][2][2] = point{0, 0}
 
